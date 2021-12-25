b7665d0d5da7900520ece221273b1154429fd81a
katina-anachkova <katianachkova@gmail.com>
2021-12-25 15:19:41 +0200

    show all games

--- src/components/GameCatalog/CatalogGame.js
+++ src/components/GameCatalog/CatalogGame.js
@@ -4,22 +4,32 @@ import CardGame from "./CardGame";
 const CatalogGame = () => {
 
     const [games, setGames] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         setTimeout(() => {
             fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
                 .then(res => res.json())
                 .then(result => {
                     setGames(result)
+                    setLoading(false)
                 });
         }, 1000)
-    }, [])
+    }, []);
+
+
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
-            {games.map(x => <CardGame game={x} />)}
-            <h3 className="no-articles">No games yet</h3>
+            {loading
+                ? <p>Loading...</p>
+                : games.length > 0
+                    ? games.map(x => <CardGame key={x._id} game={x} />)
+                    : <h3 className="no-articles">No games yet</h3>
+            }
+
         </section>
     );
 };
